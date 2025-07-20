06115b24ee7e29e447f29336f1c5b0b044774711
Tejasva Gupta <tejas24gupta@gmail.com>
2025-07-21 00:29:58 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,6 @@
 #include "include/csvparser.hpp"
 
 int main(){
-    DataFrame df;
-    DataFrame new_df;
-    df.read_csv("data.csv", ',');
-    df.copy(new_df, std::vector<std::string>{"Age","Score"});  //Avoid using same DataFrame object for both reading and making a new DataFrame
-    df.print();
-    new_df.print();
-    new_df.print_info();
+    DataFrame df;    //Object Declaration
+    //Write your Code Here
 }
